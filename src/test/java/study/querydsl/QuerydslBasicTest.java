debc6b26f8cc884d3bc9939b0f08370a1e9d0388
lhw7511 <lhw751@gmail.com>
2022-11-17 16:01:14 +0900

    case문 처리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -359,6 +360,34 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void basicCase(){
+        List<String> result = jpaQueryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타")
+                )
+                .from(member)
+                .fetch();
+        result.forEach(r->System.out.println(r));
+    }
+
+
+    @Test
+    public void complexCase(){
+        List<String> result = jpaQueryFactory
+                .select(
+                        new CaseBuilder()
+                                .when(member.age.between(0, 20)).then("0~20살")
+                                .when(member.age.between(21, 30)).then("21~30살")
+                                .otherwise("기타")
+                )
+                .from(member)
+                .fetch();
+
+        result.forEach(r->System.out.println(r));
+    }
 
 
 }
