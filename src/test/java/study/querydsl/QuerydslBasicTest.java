fe02d01785102a37d34d08f4da2ade766c54e2d2
lhw7511 <lhw751@gmail.com>
2022-11-21 17:46:37 +0900

    sql function 호출

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -575,4 +575,34 @@ public class QuerydslBasicTest {
                 .where(member.age.gt(18))
                 .execute();
     }
+
+    @Test
+    public void sqlFunction(){
+        List<String> result = jpaQueryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace',{0},{1},{2})",
+                        member.username, "member", "M")
+                )
+                .from(member)
+                .fetch();
+
+        for(String s : result){
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2(){
+        List<String> result = jpaQueryFactory
+                .select(member.username)
+                .from(member)
+                //.where(member.username.eq(Expressions.stringTemplate("function('lower',{0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for(String s : result){
+            System.out.println(s);
+        }
+
+    }
 }
