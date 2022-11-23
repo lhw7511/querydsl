4dd79439563f3ce38f776e7ddea544b10610eaca
lhw7511 <lhw751@gmail.com>
2022-11-23 09:54:53 +0900

    동적쿼리 레포지토리

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -13,10 +13,4 @@ public class QuerydslApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
-
-	@Bean
-	JPAQueryFactory jpaQueryFactory(EntityManager em){
-		return new JPAQueryFactory(em);
-	}
-
 }
